ac6db981003384d00b94b582ccf50b9683e47d41
Vineeth <vineeth122@yahoo.co.in>
2023-04-02 18:53:06 -0400

    first commit

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,35 @@
+export default [{
+    "_id": 245,
+    "userName": "Elon Musk",
+    "handle": "@elonmusk",
+    "time": "23h",
+    "body": "Amazing show about @Inspiration4x mission!",
+    "title": "Countdown: Inspirational4 Mission to Space | Netflix Official Site",
+    "image": "Teslaicon.png",
+    "postimage": "astro.png",
+    "subtitle": "From training to launch to landing, this all-access docu series rides along with the Inspirational4 crew on the first all-civilian orbital space ..",
+    "link": "netflix.com",
+    "comment": 123,
+    "retweet": 432,
+    "like": 2345,
+    "liked": false,
+    "dislike": 23
+},
+    {
+        "_id": 345,
+        "userName": "New York Post",
+        "handle": "@nypost",
+        "time": "23h",
+        "title": "",
+        "body": "Grimes seen reading Karl Marx following split with world's richest man Elon Musk trib.al / nx2Gfaq ",
+        "image": "nypost.png",
+        "postimage": "grimes.png",
+        "subtitle": "",
+        "link": "",
+        "comment": 966,
+        "retweet": 2365,
+        "like": 4321,
+        "liked": true,
+        "dislike": 46
+    }
+];
