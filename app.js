dd500c39c613a52d6c2f8a976432784c16bd0d18
vineeth <vineeth122@yahoo.co.in>
2023-04-08 23:54:40 -0400

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
 import cors from 'cors';
 import mongoose from "mongoose";
-// mongoose.connect('mongodb+srv://vineeth8472:<password>@cluster0.sycmfry.mongodb.net/?retryWrites=true&w=majority');
+
 
 const CONNECTION_STRING = process.env.DB_CONNECTION_STRING || 'mongodb://127.0.0.1:27017/tuiter';
 mongoose.connect(CONNECTION_STRING);
